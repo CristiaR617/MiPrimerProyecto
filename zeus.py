847d2582259b157b4efed4426aabfe209a6786c9
CristiaR617 <129903043+CristiaR617@users.noreply.github.com>
2023-04-04 17:19:44 -0500

    cambios para mejor visualizacion

--- zeus.py
+++ zeus.py
@@ -3,10 +3,8 @@ def suma (n1,n2):
     sum = n1+n2
     return sum
 print(suma(3, 4))
-# realizar una funcion que reste dos numeros   rama->resta
+
 # realizar una funcion que reste dos numeros   rama->resta
 def resta(a, b):
     return a-b
-
-
 print(resta(5, 6))
