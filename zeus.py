340a7f23c88b5cfbb2f18f460402a25d030d732e
mayer malpud <129673098+1201104027@users.noreply.github.com>
2023-04-04 17:08:42 -0500

    funcion suma realizada

--- zeus.py
+++ zeus.py
@@ -1,2 +1,6 @@
 # realizar una funcion que sume dos numeros   rama->suma
+def suma (n1,n2):
+    sum = n1+n2
+    return sum
+print(suma(3, 4))
 # realizar una funcion que reste dos numeros   rama->resta
